6d30dfdf48a43383dadb395d52380363f84a5684
Raj Rasane <rasaneraj04@gmail.com>
2024-08-07 14:59:43 +0530

    new methods added displaying more patterns

--- JavaPatternsUsingLoops.java
+++ JavaPatternsUsingLoops.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class JavaPatternsUsingLoops {
     public static void main(String[] args) {
-        pattern10();
+        pattern15();               
     }
 
     // Pattern 1
@@ -55,7 +55,7 @@ public class JavaPatternsUsingLoops {
 
         System.out.println("Pattern :- ");
 
-        for(i=0;i<=n;i++){
+        for(i=0;i<n;i++){
             for(j=0;j<n-i;j++){
                 System.out.print("* ");
             }
@@ -202,6 +202,144 @@ public class JavaPatternsUsingLoops {
                 else{
                     System.out.print(0);
                 }
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern 11 (Butterfly pattern)
+    public static void pattern11(int n){
+        int i,j;
+        // Scanner sc =new Scanner(System.in);
+
+        // System.out.println("Enter number for how many lines of pattern you want to display:- ");
+        // n = sc.nextInt();
+
+        System.out.println("pattern :- ");
+        // 1st half
+        for(i=1;i<=n;i++){
+            // stars - i
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            // spaces - 2*(n-i)
+            for(j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+
+            // stars - i
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 2nd half
+        for(i=n;i>=1;i--){
+            // stars - i
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            // spaces - 2*(n-i)
+            for(j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+
+            // stars - i
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern 12 (Solid Rhombus Pattern)
+    public static void pattern12(){
+        int i,j;
+        int n = 5;
+
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern 13 (Hollow Rhombus Pattern)
+    public static void pattern13(){
+        int i,j;
+        int n = 5;
+
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern 14 - (Diamond Pattern)
+    public static void pattern14(){
+        int i,j,n=4;
+
+        // 1st half
+        for(i=1;i<=n;i++){
+            // spaces
+            for(j=n-i;j>=1;j--){
+                System.out.print(" ");
+            }
+
+            // stars
+            for(j=1;j<=2*(i-1)+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 2nd half
+        for(i=n;i>=1;i--){
+            // spaces
+            for(j=n-i;j>=1;j--){
+                System.out.print(" ");
+            }
+
+            // stars
+            for(j=1;j<=2*(i-1)+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern 15 - (Numbered Half Diamond)
+    public static void pattern15(){
+        int i,j;
+        int n=5;
+
+        for(i=1;i<=n;i++){
+            for(j=n-i;j>=1;j--){
+                System.out.print("  ");
+            }
+            for(j=i;j>=1;j--){
+                System.out.print(j + " ");
+            }
+            for(j=2;j<=i;j++){
+                System.out.print(j + " ");
             }
             System.out.println();
         }
